7a1040843ab3f77c816a086df640f7fd69249ba8
marcus <marcusvcf04@gmail.com>
2026-06-05 10:53:47 -0300

    Add UUIDv7 tests

---
+++ src/test/java/com/algaworks/algasensors/temperature/processing/UUIDv7Test.java
@@ -0,0 +1,22 @@
+package com.algaworks.algasensors.temperature.processing;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.OffsetDateTime;
+import java.time.temporal.ChronoUnit;
+
+class UUIDv7Test {
+
+    @Test
+    void shouldGenerateUUIDv7() {
+        var uuid = IdGenerator.generateTimeBasedUUID();
+
+
+        OffsetDateTime uuidDateTime = UUIDv7Utils.extractOffSetDateTime(uuid).truncatedTo(ChronoUnit.MINUTES);
+        OffsetDateTime currentDateTime = OffsetDateTime.now().truncatedTo(ChronoUnit.MINUTES);
+
+        Assertions.assertThat(uuidDateTime).isEqualTo(currentDateTime);
+    }
+
+}
