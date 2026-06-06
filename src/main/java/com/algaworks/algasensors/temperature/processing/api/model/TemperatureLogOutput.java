cbd06854b866b19cb8e2650ef34770257b7061e6
marcus <marcusvcf04@gmail.com>
2026-06-05 22:57:42 -0300

    Fix imports and data types

--- src/main/java/com/algaworks/algasensors/temperature/processing/api/model/TemperatureLogOutput.java
+++ src/main/java/com/algaworks/algasensors/temperature/processing/api/model/TemperatureLogOutput.java
@@ -2,13 +2,13 @@ package com.algaworks.algasensors.temperature.processing.api.model;
 
 import io.hypersistence.tsid.TSID;
 
-import java.time.OffsetTime;
+import java.time.OffsetDateTime;
 import java.util.UUID;
 
 public record TemperatureLogOutput(
         UUID id,
         TSID sensorId,
-        OffsetTime registeredAt,
+        OffsetDateTime registeredAt,
         Double value
 ) {
 }
